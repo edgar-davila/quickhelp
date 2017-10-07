f7b366bd43a340f60762c661c993f7f507b7e33e
Jose Larreal <jdla1990@gmail.com>
2017-10-07 14:32:48 -0300

    Primer commit

---
+++ app/src/main/java/com/omsuperg/quickhelp/LoginActivity.java
@@ -0,0 +1,78 @@
+package com.omsuperg.quickhelp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class LoginActivity
+        extends AppCompatActivity
+        implements FacebookCallback<LoginResult> {
+
+    private CallbackManager callbackManager;
+    private String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        if (AccessToken.getCurrentAccessToken() != null) {
+            Log.d(TAG, "onCreate: Hay una session iniciada");
+            startMainActivity(AccessToken.getCurrentAccessToken());
+        }
+
+
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList( "public_profile", "email"));
+        loginButton.registerCallback(callbackManager, this);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSuccess(LoginResult loginResult) {
+        startMainActivity(loginResult.getAccessToken());
+    }
+
+    private void startMainActivity(AccessToken accessToken) {
+        startActivity(new Intent(this, MainActivity.class)
+                .putExtra("TokenSession", accessToken));
+        finish();
+    }
+
+    @Override
+    public void onCancel() {
+        Toast.makeText(this, "Se cancelo", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onError(FacebookException error) {
+        Toast.makeText(this, "Se produjo un error", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/omsuperg/quickhelp/MainActivity.java
@@ -0,0 +1,151 @@
+package com.omsuperg.quickhelp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.omsuperg.quickhelp.transforms.CircleTransform;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+    private AccessToken accessToken;
+
+    ImageView imageViewFacebookProfile;
+    TextView textviewNameProfile;
+    TextView textviewEmailProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View headerMenuDrawable = navigationView.getHeaderView(0);
+        imageViewFacebookProfile = headerMenuDrawable.findViewById(R.id.imageViewFacebookProfile);
+        textviewNameProfile = headerMenuDrawable.findViewById(R.id.textviewNameProfile);
+        textviewEmailProfile = headerMenuDrawable.findViewById(R.id.textviewEmailProfile);
+
+        this.accessToken = getIntent().getParcelableExtra("TokenSession");
+        Log.d(TAG, "onCreate: " + this.accessToken.toString());
+
+        getProfileFromFacebook();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.menuItemLogout) {
+            LoginManager.getInstance().logOut();
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    private void getProfileFromFacebook() {
+        GraphRequest request = GraphRequest.newMeRequest(
+                AccessToken.getCurrentAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.v(TAG, response.toString());
+
+                        try {
+
+                            textviewNameProfile.setText(object.getString("name"));
+                            textviewEmailProfile.setText(object.getString("email"));
+                            Picasso.with(MainActivity.this)
+                                    .load("https://graph.facebook.com/" + object.getString("id") + "/picture?type=large")
+                                    .transform(new CircleTransform())
+                                    .into(imageViewFacebookProfile);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+}
